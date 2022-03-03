31394871681ffc9b9fa7b73c2e945558d13c4b97
Cruseider1 <97914351+Cruseider1@users.noreply.github.com>
2022-03-03 19:02:34 +0100

    Update generator_haseł.py

--- generator_haseł.py
+++ generator_haseł.py
@@ -1,1 +1,5 @@
-print (xd asdas)
+import random #biblioteka do losowania
+import string #biblioteka znaków specjalnych
+
+lowercase = string.ascii_lowercase #znaki alfabetu małe
+uppercase = string.ascii_uppercase #znaki alfabetu duże
