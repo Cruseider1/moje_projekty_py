8cb384918a7940b0b015c41754383ec930c7eb95
Cruseider1 <wisniewski.sic@gmail.com>
2022-03-03 21:48:55 +0000

    zmiana kodu na średnie hasło - losowe 8 znaków

--- generator_hasel.py
+++ generator_hasel.py
@@ -1,7 +1,23 @@
 import random #biblioteka do losowania
 import string #biblioteka znaków specjalnych
+import itertools
 
 lowercase = string.ascii_lowercase #znaki alfabetu małe
 uppercase = string.ascii_uppercase #znaki alfabetu duże
+digits = string.digits #dodaje cyfry
+punctuation = string.punctuation #dodaje znaki specjalne
 
-print("abc")
+n_znakow = 8
+#n_znakow_typu = 2
+
+wszystkie_znaki = random.sample((lowercase, 2) + (uppercase, 2) + (digits, 2) + (punctuation, 2))
+
+losowe_znaki = random.sample(wszystkie_znaki, n_znakow) #generuje 8 różnych znaków ze zmiennej
+
+
+#hasło to ciąg znaków (stringi)
+haslo = ''.join(losowe_znaki) #powoduje że znaki hasła nie będą przedzielone jak stringi znakiem ''
+
+print(haslo)
+
+ 
