87994999336d5e8a75171beb83aaf8c1bb24b10f
Cruseider1 <wisniewski.sic@gmail.com>
2022-03-06 14:46:46 +0000

    ostateczna wersja generatora haseł po 2 znaki z każdej paczki znaków

--- generator_hasel.py
+++ generator_hasel.py
@@ -1,6 +1,6 @@
 import random #biblioteka do losowania
 import string #biblioteka znaków specjalnych
-import itertools
+
 
 lowercase = string.ascii_lowercase #znaki alfabetu małe
 uppercase = string.ascii_uppercase #znaki alfabetu duże
@@ -10,7 +10,7 @@ punctuation = string.punctuation #dodaje znaki specjalne
 n_znakow = 8
 #n_znakow_typu = 2
 
-wszystkie_znaki = random.sample((lowercase, 2) + (uppercase, 2) + (digits, 2) + (punctuation, 2))
+wszystkie_znaki = random.sample(lowercase, 2) + random.sample(uppercase, 2) + random.sample(digits, 2) + random.sample(punctuation, 2)
 
 losowe_znaki = random.sample(wszystkie_znaki, n_znakow) #generuje 8 różnych znaków ze zmiennej
 
