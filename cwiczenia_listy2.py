63c3c3a4e58cef93cebcbd1a6438e349d9361650
Cruseider1 <wisniewski.sic@gmail.com>
2022-03-18 09:06:10 +0000

    ćwiczenia listy min/max

--- cwiczenia_listy2.py
+++ cwiczenia_listy2.py
@@ -2,5 +2,17 @@
 #tupla = (1, 2, 3)
 #sety = {1, 2, 3}
 
-lista = ['a', 1, True] #lista różnych typów
-print('Lista jest typu:', type(lista) ) # komenda sprawdzająca typ danych
+#lista = ['a', 1, True] #lista różnych typów
+#print('Lista jest typu:', type(lista) ) # komenda sprawdzająca typ danych
+
+lista = [1,2,3,4,5]
+dlugosc_listy = len(lista)
+najmniejszy_element = min(lista)
+najwiekszy_element = max(lista)
+
+
+print ('lista:', lista) #printuje całą listę
+print ('dlugosc listy to', dlugosc_listy) #określa jej długość
+print ('najmniejszy element to', najmniejszy_element)
+print ('największy element to', najwiekszy_element)
+
