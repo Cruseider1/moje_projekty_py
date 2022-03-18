d95b2bb9fa5e1790978dc0c256de678d6b4d2568
Cruseider1 <wisniewski.sic@gmail.com>
2022-03-18 09:57:10 +0000

    uporządkowanie kodu

--- cwiczenia_listy2.py
+++ cwiczenia_listy2.py
@@ -4,15 +4,17 @@
 
 #lista = ['a', 1, True] #lista różnych typów
 #print('Lista jest typu:', type(lista) ) # komenda sprawdzająca typ danych
+#--------------------------------------------------------------------------
+#lista = [1,2,3,4,5]
+#dlugosc_listy = len(lista)
+#najmniejszy_element = min(lista)
+#najwiekszy_element = max(lista)
 
-lista = [1,2,3,4,5]
-dlugosc_listy = len(lista)
-najmniejszy_element = min(lista)
-najwiekszy_element = max(lista)
+#print ('lista:', lista) #printuje całą listę
+#print ('dlugosc listy to', dlugosc_listy) #określa jej długość
+#print ('najmniejszy element to', najmniejszy_element)
+#print ('największy element to', najwiekszy_element)
+#-------------------------------------------------------------------------
 
-
-print ('lista:', lista) #printuje całą listę
-print ('dlugosc listy to', dlugosc_listy) #określa jej długość
-print ('najmniejszy element to', najmniejszy_element)
-print ('największy element to', najwiekszy_element)
-
+set = {1,2,3,4,5}
+print (set, type(set))
