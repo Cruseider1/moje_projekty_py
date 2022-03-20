98da534fb75c863d24555796f52e3978b977dc14
Cruseider1 <wisniewski.sic@gmail.com>
2022-03-20 10:58:00 +0000

    indeksy ćwiczenie

---
+++ Indeksy_cwiczenia.py
@@ -0,0 +1,10 @@
+
+#'a' (indeks 0),
+#'b' (indeks 1),
+#'c' (indeks 2),
+#lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g'] #, 'h', 'i', 'j', 'k', 'l']
+#i = 0 # indeks - i
+#element = lista[i]
+
+#print(element) #teraz indeks jest ustawiony na element '0' więc wydrukuje literę 'a'
+#---------------------------------------------------------------------
