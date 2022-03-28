622621a051c73e311d4729671dee6e9cda54b77c
Cruseider1 <wisniewski.sic@gmail.com>
2022-03-28 18:07:33 +0000

    instrukcje warunkowe

--- input_ciwczenia.py
+++ input_ciwczenia.py
@@ -19,8 +19,22 @@
 
 #-----------------------------------------------------------  
 
-password = "xxx"
+# password = "xxx"
 
-while password != "trzaslo":
-    hasło = input('podaj hasło:  ')
-    print = ('udało Ci się, brawo')
+# while password != "trzaslo":
+#     hasło = input('podaj hasło:  ')
+# print = ('udało Ci się, brawo') # w tym kodzie coś nie pykło ciągle pyta o hasło nawet jak warunek jest spełniony
+
+#-----------------------------------------------------------  
+# instrukcja warunkowa czyli inaczej ify / elify i elsy inaczej na polski - jeżeli. Pozwala na wykonanie zestawy poleceń jeśli jest spełniony w niej warunek, jest podobieństwo do while w tej funkcji z tym że to nie jest pętla tylko instrukcja warunkowa więc z założenia polecenia w niej zawarte wykonują się jednokrotnie. 
+
+# a = input('podaj pierwszą liczbę:  ')
+# b = input('podaj drugą liczbę:  ')
+
+# if a>b:
+#     print('pierwsza liczba jest większa')
+# elif b>a:
+#     print('druga liczba jest większa')
+# else:
+#     print('liczby są równe')
+#----------------------------------------------------------- 
