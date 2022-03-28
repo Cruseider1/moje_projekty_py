9f4c76db314109b3bc2a8e580a99e5188b28e176
Cruseider1 <wisniewski.sic@gmail.com>
2022-03-28 17:52:55 +0000

    cwiczenia z pętlami

---
+++ input_ciwczenia.py
@@ -0,0 +1,26 @@
+# name = input('jak masz na imie?: ')
+# print ("miło Cię poznać  " +name)
+
+#Dwa rodzaje pętli to fory (robi pętlę określoną liczbę razy ) i while (robi pętlę dopóki nie zostanie spełniony warunek)
+
+#-----------------------------------------------------------  
+# for i in range(100):
+#     print(i) #ten krotki kod mówi - wydrukuj w zakresie 100 zmienną i python przydziela jej w tym wypadku wartość od 0-99. Z każdym kolejnym wykonaniem tej pętli jej wartość będzie rosła o 1.
+
+#-----------------------------------------------------------  
+# a = 10
+# b = 20
+
+# while a<b:
+#     a = a*2+2
+#     b = b+a
+#     print(a)
+#     print(b) #wyprintuje wielokrotności tych liczb pewnie po chwili spowodowałoby to, że serwer padnie. 
+
+#-----------------------------------------------------------  
+
+password = "xxx"
+
+while password != "trzaslo":
+    hasło = input('podaj hasło:  ')
+    print = ('udało Ci się, brawo')
