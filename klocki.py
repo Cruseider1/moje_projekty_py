00ce34acf0c580c4011c9ed978a2d0088fc75599
Cruseider1 <wisniewski.sic@gmail.com>
2022-04-10 08:52:13 +0000

    ify

--- klocki.py
+++ klocki.py
@@ -1,8 +1,24 @@
-a = 'XD'
-b = (1,2,3)
-c = [1,2,3]
-d = 1.2
+# a = 'XD'
+# b = (1,2,3)
+# c = [1,2,3]
+# d = 1.2
 
-print (a.lower())  #drukuje małymi literami stringi można uzyć też .upper i wszystkie będą powiększone
+# print (a.lower())  #drukuje małymi literami stringi można uzyć też .upper i wszystkie będą powiększone
+
+#------------------------------------------------------
+# x = int('1') # pozmieniaj typ.
+# if isinstance(x, int):
+#     print('x to int', x, type(x))
+
+#------------------------------------------------------
+
+# x = 2
+
+# print('czy to prawda że x = 1?')
+
+# if x == 1:
+#     print("tak to prawda")
+# else:
+#     print('to inna wartość')
 
 #------------------------------------------------------
