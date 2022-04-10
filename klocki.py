ef71b43fadbca19f117b316ffe9ddf21650d8299
Cruseider1 <wisniewski.sic@gmail.com>
2022-04-10 08:14:56 +0000

    zabawa pythonem

---
+++ klocki.py
@@ -0,0 +1,8 @@
+a = 'XD'
+b = (1,2,3)
+c = [1,2,3]
+d = 1.2
+
+print (a.lower())  #drukuje małymi literami stringi można uzyć też .upper i wszystkie będą powiększone
+
+#------------------------------------------------------
